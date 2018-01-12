4c49c1185e1eb7ad91696af54543d38dce1526b8
Rafal Bereski <rbereski@live.com>
2018-01-13 00:57:15 +0100

    Update for Swift 4

--- NativeUI/NativeUI/AppDelegate.swift
+++ NativeUI/NativeUI/AppDelegate.swift
@@ -10,11 +10,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
-    private func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         // Override point for customization after application launch.
         return true
     }
@@ -40,7 +38,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
